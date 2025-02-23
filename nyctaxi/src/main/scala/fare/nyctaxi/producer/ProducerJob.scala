d092e4b67f0f96b1ee54e7b87cbd02a14c4997de
vianez <tiagovianez@live.com>
2025-02-22 21:10:20 -0300

    Small fix ProducerJob

--- nyctaxi/src/main/scala/fare/nyctaxi/producer/ProducerJob.scala
+++ nyctaxi/src/main/scala/fare/nyctaxi/producer/ProducerJob.scala
@@ -4,10 +4,8 @@ import org.apache.spark.sql.{SparkSession, DataFrame}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.streaming.Trigger
-import java.util.Properties
 import org.apache.log4j.{Level, Logger}
 import fare.nyctaxi.Constants
-
 
 object ProducerJob {
   def main(args: Array[String]): Unit = {
@@ -23,23 +21,26 @@ object ProducerJob {
       .master("local[3]")
       .getOrCreate()
 
-//    import spark.implicits._
+    import spark.implicits._
 
     val df = spark.read
-      .option("header", "true")
-      .schema(Constants.taxiSchema)
-      .csv(Constants.SOURCE_DATA_PATH)
+      .format("parquet")
+      .load(Constants.SOURCE_PARQUET_PATH)
+
 
     val kafkaMessages = df.select(
       col("key").cast(StringType).alias("key"),
       to_json(struct(
         col("fare_amount").cast(FloatType).alias("fare_amount"),
-        col("pickup_datetime").cast(TimestampNTZType).alias("pickup_datetime"),
+        col("pickup_datetime").cast(TimestampType).alias("pickup_datetime"),
         col("pickup_longitude").cast(DecimalType(8,6)).alias("pickup_longitude"),
         col("pickup_latitude").cast(DecimalType(8,6)).alias("pickup_latitude"),
         col("dropoff_longitude").cast(DecimalType(8,6)).alias("dropoff_longitude"),
         col("dropoff_latitude").cast(DecimalType(8,6)).alias("dropoff_latitude"),
-        col("passenger_count").cast(ByteType).alias("passenger_count")
+        col("passenger_count").cast(ByteType).alias("passenger_count"),
+        col("year").cast(ShortType).alias("year"),
+        col("month").cast(ShortType).alias("month"),
+        col("day").cast(ShortType).alias("day")
       )).alias("value")
     )
 
