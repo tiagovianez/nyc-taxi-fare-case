12d3e39165062359d30ba7adef5a3aabf7e101ad
vianez <tiagovianez@live.com>
2025-02-22 21:11:01 -0300

    Fix variables paths and Structure fields schema

--- nyctaxi/src/main/scala/fare/nyctaxi/Constants.scala
+++ nyctaxi/src/main/scala/fare/nyctaxi/Constants.scala
@@ -6,13 +6,15 @@ import org.apache.spark.sql.types._
 object Constants {
   private val config: Config = ConfigFactory.load("NycTaxiFare.conf") // Carrega o arquivo de configuração
 
-  val SOURCE_DATA_PATH: String = config.getString("Sources.inputData")
+  val SOURCE_CSV_PATH: String = config.getString("Sources.inputCsvData")
+  val SOURCE_PARQUET_PATH: String = config.getString("Sources.inputParquetData")
   val NEIGHBORHOOD_PATH: String = config.getString("Sources.neighborhoodPath")
 
   val RAW_DELTA_PATH: String = config.getString("RawDelta.path")
-  val CHECKPOINTS_PATH: String = config.getString("RawDelta.checkpoints")
+  val CHECKPOINTS_RAW_PATH: String = config.getString("RawDelta.checkpointsRaw")
 
-  val CURATED_PARQUET_PATH: String = config.getString("CuratedParquet.out")
+  val CURATED_DELTA_PATH: String = config.getString("CuratedDelta.out")
+  val CHECKPOINTS_CURATED_PATH: String = config.getString("CuratedDelta.checkpointsCurated")
 
 
   val log4jConfigPath = "src/main/resources/log4j.properties"
@@ -21,14 +23,25 @@ object Constants {
   val kafkaTopic = "nyc-taxi-rides"
 
 
+  val taxiSchema = StructType(Array(
+    StructField("key", StringType, nullable = true),
+    StructField("fare_amount", FloatType, nullable = true),
+    StructField("pickup_datetime", TimestampType, nullable = true),
+    StructField("pickup_longitude", DoubleType, nullable = true),
+    StructField("pickup_latitude", DoubleType, nullable = true),
+    StructField("dropoff_longitude", DoubleType, nullable = true),
+    StructField("dropoff_latitude", DoubleType, nullable = true),
+    StructField("passenger_count", ByteType, nullable = true)
+  ))
+
   val rawSchema = StructType(Array(
     StructField("key", StringType, nullable = false),
     StructField("fare_amount", FloatType, nullable = true),
-    StructField("pickup_datetime", TimestampNTZType, nullable = true),
-    StructField("pickup_longitude", DecimalType(8,6), nullable = true),
-    StructField("pickup_latitude", DecimalType(8,6), nullable = true),
-    StructField("dropoff_longitude", DecimalType(8,6), nullable = true),
-    StructField("dropoff_latitude", DecimalType(8,6), nullable = true),
+    StructField("pickup_datetime", TimestampType, nullable = true),
+    StructField("pickup_longitude", DoubleType, nullable = true),
+    StructField("pickup_latitude", DoubleType, nullable = true),
+    StructField("dropoff_longitude", DoubleType, nullable = true),
+    StructField("dropoff_latitude", DoubleType, nullable = true),
     StructField("passenger_count", ByteType, nullable = true),
     StructField("year", ShortType, nullable = true),
     StructField("month", ByteType, nullable = true),
@@ -41,4 +54,6 @@ object Constants {
     StructField("latitude", DecimalType(8,6), nullable = true),
     StructField("longitude", DecimalType(8,6), nullable = true)
   ))
+
+
 }
